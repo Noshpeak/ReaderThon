5361cb1401fde3761a87eee2268e8000b21ef1b3
Noshpeak <110942905+Noshpeak@users.noreply.github.com>
2023-03-10 12:47:14 -0500

    Login, Register completed

---
+++ RegisterView.swift
@@ -0,0 +1,199 @@
+//
+//  RegisterView.swift
+//  ReaderThon
+//
+//  Created by Lingeswaran Kandasamy on 3/10/23.
+//
+
+import SwiftUI
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseStorage
+import PhotosUI
+
+// MARK: Register View
+struct RegisterView: View{
+    // MARK: User Details
+    @State var emailID: String = ""
+    @State var password: String = ""
+    @State var userRole: String = ""
+    @State private var showPassword = false
+  
+    @State var userProfilePicData: Data?
+    // MARK: View Properties
+    @Environment(\.dismiss) var dismiss
+    @State var showImagePicker: Bool = false
+    @State var photoItem: PhotosPickerItem?
+    @State var showError: Bool = false
+    @State var errorMessage: String = ""
+    @State var isLoading: Bool = false
+    // MARK: UserDefaults
+    @AppStorage("log_status") var logStatus: Bool = false
+    @AppStorage("user_profile_url") var profileURL: URL?
+    @AppStorage("user_name") var userNameStored: String = ""
+    @AppStorage("user_UID") var userUID: String = ""
+    var body: some View{
+        VStack(spacing: 10){
+            Text("Lets Register\nAccount")
+                .font(.largeTitle.bold())
+                .hAlign(.leading)
+            
+            Text("Hello Parents, Thanks for register ")
+                .font(.title3)
+                .hAlign(.leading)
+            
+            // MARK: For Smaller Size Optimization
+            ViewThatFits {
+                ScrollView(.vertical, showsIndicators: false) {
+                    HelperView()
+                }
+                
+                HelperView()
+            }
+            
+            // MARK: Register Button
+            HStack{
+                Text("Already Have an account?")
+                    .foregroundColor(.gray)
+                
+                Button("Login Now"){
+                    dismiss()
+                }
+                .fontWeight(.bold)
+                .foregroundColor(.black)
+            }
+            .font(.callout)
+            .vAlign(.bottom)
+        }
+        .vAlign(.top)
+        .padding(15)
+        .overlay(content: {
+            LoadingView(show: $isLoading)
+        })
+        .photosPicker(isPresented: $showImagePicker, selection: $photoItem)
+        .onChange(of: photoItem) { newValue in
+            // MARK: Extracting UIImage From PhotoItem
+            if let newValue{
+                Task{
+                    do{
+                        guard let imageData = try await newValue.loadTransferable(type: Data.self) else{return}
+                        // MARK: UI Must Be Updated on Main Thread
+                        await MainActor.run(body: {
+                            userProfilePicData = imageData
+                        })
+                        
+                    }catch{}
+                }
+            }
+        }
+        // MARK: Displaying Alert
+        .alert(errorMessage, isPresented: $showError, actions: {})
+    }
+    
+    @ViewBuilder
+    func HelperView()->some View{
+        VStack(spacing: 12){
+            ZStack{
+                if let userProfilePicData,let image = UIImage(data: userProfilePicData){
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                }else{
+                    Image("NullProfile")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                }
+            }
+            .frame(width: 85, height: 85)
+            .clipShape(Circle())
+            .contentShape(Circle())
+            .onTapGesture {
+                showImagePicker.toggle()
+            }
+            .padding(.top,25)
+    
+            TextField("Email", text: $emailID)
+                .textContentType(.emailAddress)
+                .border(1, .gray.opacity(0.5))
+            
+            HStack {
+                if showPassword {
+                    TextField("Password", text: $password)
+                        
+                } else {
+                    SecureField("Password", text: $password)
+                     
+                }
+                Button(action: {
+                    showPassword.toggle()
+                }) {
+                    Image(systemName: showPassword ? "eye.slash.fill" : "eye.fill")
+                        .foregroundColor(.gray)
+                }
+            }
+            .border(1, .gray.opacity(0.5))
+            
+            Button(action: registerUser){
+                // MARK: Login Button
+                Text("Sign up")
+                    .foregroundColor(.white)
+                    .hAlign(.center)
+                    .fillView(.black)
+            }
+            .disableWithOpacity(userRole == "" || emailID == "" || password == "" || userProfilePicData == nil)
+            .padding(.top,10)
+        }
+    }
+    
+    func registerUser(){
+        isLoading = true
+        closeKeyboard()
+        Task{
+            do{
+                // Step 1: Creating Firebase Account
+                try await Auth.auth().createUser(withEmail: emailID, password: password)
+                // Step 2: Uploading Profile Photo Into Firebase Storage
+                guard let userUID = Auth.auth().currentUser?.uid else{return}
+                guard let imageData = userProfilePicData else{return}
+                let storageRef = Storage.storage().reference().child("Profile_Images").child(userUID)
+                let _ = try await storageRef.putDataAsync(imageData)
+                // Step 3: Downloading Photo URL
+                let downloadURL = try await storageRef.downloadURL()
+                // Step 4: Creating a User Firestore Object
+                let user = User(userRole: userRole, userUID: userUID, userEmail: emailID, userProfileURL: downloadURL)
+                
+                // Step 5: Saving User Doc into Firestore Database
+                let _ = try Firestore.firestore().collection("Users").document(userUID).setData(from: user, completion: { error in
+                    if error == nil{
+                        // MARK: Print Saved Successfully
+                        print("Saved Successfully")
+                        self.userUID = userUID
+                        profileURL = downloadURL
+                        logStatus = true
+                    }
+                })
+            }catch{
+                // MARK: Deleting Created Account In Case of Failure
+                try await Auth.auth().currentUser?.delete()
+                await setError(error)
+            }
+        }
+    }
+    
+    // MARK: Displaying Errors VIA Alert
+    func setError(_ error: Error)async{
+        // MARK: UI Must be Updated on Main Thread
+        await MainActor.run(body: {
+            errorMessage = error.localizedDescription
+            showError.toggle()
+            isLoading = false
+        })
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
+
